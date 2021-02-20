cbebf160208a3c7691b04fc73e7bb9ffb0e69b98
Ramil Saavedra <ramilsmcl@gmail.com>
2021-02-20 21:46:04 +0800

    add button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Button from './components/Button/Button';
 import List from './components/List';
 import sampleData from './sampleData/sampleData';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
         <h1>Todo List</h1>
         <List todos={sampleData} />
+        <Button>Close Shipment</Button>
     </div>
   );
 }
